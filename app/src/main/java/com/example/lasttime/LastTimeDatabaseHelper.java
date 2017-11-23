de3df41aef11e4188ca91e8252dc5f3371b6afcf
ggrc <670141952@qq.com>
2017-11-24 00:02:12 +0800

    62th

--- app/src/main/java/com/example/lasttime/LastTimeDatabaseHelper.java
+++ app/src/main/java/com/example/lasttime/LastTimeDatabaseHelper.java
@@ -6,17 +6,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by ggrc on 2017/10/27.
- * 数据库
+ * 用于数据库建表
  */
 
 public class LastTimeDatabaseHelper extends SQLiteOpenHelper {
-    private static final String CREATE_KITH_AND_KIN = "create table KITH_AND_KIN ("
-            + "call text primary key, "
+    //亲友表，用于存储用户设置的亲友号码,以及距离设置的亲友上一次通话的时间
+    private static final String CREATE_KITH_AND_KIN_TABLE = "create table KITH_AND_KIN ("
+            +"call text primary key, "
             +"num text,"
             +"date integer,"
             +"frequency integer"
             +")";
-    private static final String CREATE_PHOTO="create table PHOTO ("
+    //图片表，用于存储用户拍照记录和通过图片记录后获取的信息，尤其是地址信息
+    private static final String CREATE_PHOTO_TABLE ="create table PHOTO ("
             +"place text primary key,"
             +"photo_path text, "
             +"date integer,"
@@ -31,6 +33,22 @@ public class LastTimeDatabaseHelper extends SQLiteOpenHelper {
             +"data text primary key,"
             +"date integer,"
             +")";
+    private static final String CREATE_POINT_IN_TIME_OF_COMMEMORATION="create table TIME ("
+            +"time integer primary key,"
+            +"frequency integer,"
+            +")";
+    private static final String CREATE_RIGHT_COMMEMORATION_FREQUENCY="create table RIGHT ("
+            +"phone integer,"
+            +"address integer,"
+            +"app integer,"
+            +"behavior integer,"
+            +")";
+    private static final String CREATE_RECORD_NUMS="create table RECORD ("
+            +"kith integer,"
+            +"photo integer,"
+            +"word integer,"
+            +"commemoration integer,"
+            +")";
     public LastTimeDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context,name,factory,version);
 
@@ -38,10 +56,14 @@ public class LastTimeDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CREATE_KITH_AND_KIN);//这个表用于存储同步通话记录后的信息
-        sqLiteDatabase.execSQL(CREATE_PHOTO);//这个表用于储存拍照记录后的信息
+        sqLiteDatabase.execSQL(CREATE_KITH_AND_KIN_TABLE);//这个表用于存储同步通话记录后的信息
+        sqLiteDatabase.execSQL(CREATE_PHOTO_TABLE);//这个表用于储存拍照记录后的信息
         sqLiteDatabase.execSQL(CREATE_WORD);//这个表用于存储手动输入记录后的信息
         sqLiteDatabase.execSQL(CREATE_COMMEMORATION);//这个表用于存储用户想被记录的信息
+        sqLiteDatabase.execSQL(CREATE_POINT_IN_TIME_OF_COMMEMORATION);
+        sqLiteDatabase.execSQL(CREATE_RIGHT_COMMEMORATION_FREQUENCY);
+        sqLiteDatabase.execSQL(CREATE_RECORD_NUMS);
+
     }
 
     @Override
--- app/src/main/java/com/example/lasttime/activity/StartActivity.java
+++ app/src/main/java/com/example/lasttime/activity/StartActivity.java
@@ -6,24 +6,16 @@ import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
 import com.example.lasttime.R;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Created by ggrc on 2017/11/13.
  */
 
 public class StartActivity extends AppCompatActivity {
-    private final static int SKIP_TIME=3300;
-    private final static int UPADATE_UI_TIME=950;
+    private final static int SKIP_TIME=2000;
     private Handler handler1 = new Handler();
-    private Handler handler2 = new Handler();
-    private TextView text;
-    private int count=3;
-    private Timer timer=new Timer();
     @Override
     protected void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
@@ -32,8 +24,6 @@ public class StartActivity extends AppCompatActivity {
         if(actionbar!=null){
             actionbar.hide();
         }
-        text = (TextView) findViewById(R.id.count_down);
-        timer.schedule(task,0,UPADATE_UI_TIME);//动态跟新倒计时TextView
         handler1.postDelayed(new Runnable() {  //使用handler的postDelayed实现延时跳转
             public void run() {
                 Intent intent = new Intent(StartActivity.this, MainActivity.class);
@@ -43,22 +33,7 @@ public class StartActivity extends AppCompatActivity {
         }, SKIP_TIME);//3秒后跳转至应用主界面MainActivity
 
     }
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
 
-                runOnUiThread(new Runnable() {      // UI thread
-                    @Override
-                    public void run() {
-                        count--;
-                        text.setText("倒计时"+count+"s");
-                        if(count ==0){
-                            timer.cancel();
-                        }
-                    }
-                });
-            }
-        };
 }
 
 
