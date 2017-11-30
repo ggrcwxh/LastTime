6dfb4be5cae08a78fa0d179dcb16c1cdec4d8413
ggrc <670141952@qq.com>
2017-11-30 23:44:19 +0800

    Refactor

--- app/src/main/java/com/example/lasttime/activity/SetKinAndKithActivity.java
+++ app/src/main/java/com/example/lasttime/activity/SetKinAndKithActivity.java
@@ -1,12 +1,15 @@
 package com.example.lasttime.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.lasttime.R;
 
 /**
- * Created by 67014 on 2017/11/30.
+ * Created by ggrcwxh on 2017/11/30.
  */
 
 public class SetKinAndKithActivity extends AppCompatActivity {
@@ -14,6 +17,22 @@ public class SetKinAndKithActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
         setContentView(R.layout.set_kin_and_kith_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar =  getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/example/lasttime/adapter/SetKinAndKithAdapter.java
@@ -0,0 +1,8 @@
+package com.example.lasttime.adapter;
+
+/**
+ * Created by 67014 on 2017/11/30.
+ */
+
+public class SetKinAndKithAdapter {
+}
