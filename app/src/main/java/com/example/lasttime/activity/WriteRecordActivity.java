40981f92e333d7329d4507b7915b2e4e99cfc652
ggrc <670141952@qq.com>
2017-12-08 21:10:11 +0800

    Refactor

--- app/src/main/java/com/example/lasttime/activity/WriteRecordActivity.java
+++ app/src/main/java/com/example/lasttime/activity/WriteRecordActivity.java
@@ -1,9 +1,13 @@
 package com.example.lasttime.activity;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.lasttime.R;
+
+import java.util.Date;
 
 /**
  * Created by 67014 on 2017/12/8.
@@ -14,5 +18,10 @@ public class WriteRecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
         setContentView(R.layout.abc_input_record);
+        TextView textView = (TextView)findViewById(R.id.input_record_text);
+        String formatType="yyyy:MM:dd HH:mm";
+        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat(formatType);
+        Date date =new Date();
+        textView.setText(simpleDateFormat.format(date));
     }
 }
